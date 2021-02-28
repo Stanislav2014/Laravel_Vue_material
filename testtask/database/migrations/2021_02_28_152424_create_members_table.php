6e88f0cf31076286ff5ac16d8770c4231303d68f
Stanislav <s_admin@mail.ru>
2021-02-28 21:21:04 +0300

    Add model and migration

---
+++ testtask/database/migrations/2021_02_28_152424_create_members_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone');
+            $table->integer('amount_invest');
+            $table->string('email');
+            $table->string('password');
+            $table->boolean('agree');
+            $table->boolean('subscribe');
+            $table->boolean('on_vacation');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
